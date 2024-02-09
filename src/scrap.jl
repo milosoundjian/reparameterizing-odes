c67ca1a226dd00291e0a263b5c458b9850053d5a
Milos Oundjian <milosoundjian@gmail.com>
2024-02-09 15:03:15 +0100

    Some work on the report

--- src/scrap.jl
+++ src/scrap.jl
@@ -1,4 +1,15 @@
-R, lambda = polynomial_ring(QQ, ["mu", "nu"])
-P, z = polynomial_ring(QQ, 5, "z")
+using Nemo
 
-lambda[1] * z[1]
+# Verhulst Model of Logistic Growth
+# dn/dt = r * n * (1 - n/k)
+
+# Let us get the Laurent Polynomial
+R, (r, k, t, n) = laurent_polynomial_ring(QQ, ["r", "k", "t", "n"])
+
+F = [r * t, r * k^(-1) * t * n]
+
+S, (r1, k1, t1, n1) = laurent_polynomial_ring(QQ, ["r1", "k1", "t1", "n1"])
+
+v = [r1 * 0 + 1, k1, t1, n1]
+
+result = [evaluate(F[i], v) for i in 1:2]
